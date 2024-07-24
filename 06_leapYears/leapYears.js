4c49193719d1e98cc84115f660efad81dbc2bc5b
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-07-24 15:01:10 -0600

    Completed Necessary Excercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisible4 =  year % 4 === 0;
+    const isDivisible100 = year % 100 === 0;
+    const isDivisuble400 = year % 400 === 0;
 
+    if (isDivisible4 && (!isDivisible100 || isDivisuble400)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
