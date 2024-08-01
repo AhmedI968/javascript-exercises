363f4e8bef63c1df190d2c9c1b8286396fde0dfd
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-08-01 08:46:04 -0600

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    let count
+    if (typeof arg !== 'number') {
+        count = parseInt(arg);
+    } else {
+        count = arg;
+    }
 
+    if (count < 0) {
+        return 'OOPS';
+    }
+
+    if (count === 0) {
+        return 0;
+    }
+
+    if (count === 1) {
+        return 1;
+    }
+
+    let fib = [0, 1];
+    for (let i = 2; i <= count; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[count];
 };
 
 // Do not edit below this line
