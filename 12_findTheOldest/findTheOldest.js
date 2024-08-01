c60d21bee010cebd052ba9096c1a91eb8439aa20
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-08-01 08:53:03 -0600

    Completed exercise 12 now finished all the exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const getAge = function(person) {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+
+    const oldest = people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest);
+        const currentAge = getAge(currentPerson);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
+
+    return oldest;
 
 };
 
